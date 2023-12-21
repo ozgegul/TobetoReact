7a57300451ea5028c626a7b4ea9064f244bbe4b1
ozgegul <ozgegul1@icloud.com>
2023-12-21 23:01:02 +0300

    Todo App First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (i) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(i, 1);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+          placeholder="Enter a new task:"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, i) => (
+          <li key={i}>
+            <span>{task}</span>
+            <button onClick={() => deleteTask(i)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
